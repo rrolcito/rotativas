fef68d62e122f62bb65d82a21a6112f741ae9866
rrolcito <rrolcito@gmail.com>
2026-07-21 01:12:13 +0200

    Update index.js

--- worker/index.js
+++ worker/index.js
@@ -1,8 +1,19 @@
+import imagenes from "../generated/imagenes.json";
+
 export default {
-  async fetch() {
+  async fetch(request) {
+
+    const personaje = "Deja";
+
+    const lista = imagenes[personaje].avatar;
+
+    const imagen =
+      lista[Math.floor(Math.random() * lista.length)];
 
     const respuesta = await fetch(
-      "https://rotativas.vercel.app/Deja/avatar/AvatarDeja.png"
+
+      `https://rotativas.vercel.app/${personaje}/avatar/${imagen}`
+
     );
 
     return new Response(respuesta.body, {
