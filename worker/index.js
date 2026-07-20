19edae213a36186734aa61706b8be5126b6230a1
rrolcito <rrolcito@gmail.com>
2026-07-21 01:14:43 +0200

    Update index.js

--- worker/index.js
+++ worker/index.js
@@ -1,9 +1,28 @@
 import imagenes from "../generated/imagenes.json";
 
 export default {
+
   async fetch(request) {
 
-    const personaje = "Deja";
+    const url = new URL(request.url);
+
+    let personaje = url.pathname.slice(1);
+
+    if (!personaje) {
+      return new Response("Indica un personaje", {
+        status: 400
+      });
+    }
+
+    personaje = personaje.replace(".png", "");
+
+    if (!imagenes[personaje]) {
+
+      return new Response("Personaje no encontrado", {
+        status:404
+      });
+
+    }
 
     const lista = imagenes[personaje].avatar;
 
@@ -11,17 +30,18 @@ export default {
       lista[Math.floor(Math.random() * lista.length)];
 
     const respuesta = await fetch(
-
       `https://rotativas.vercel.app/${personaje}/avatar/${imagen}`
-
     );
 
     return new Response(respuesta.body, {
+
       headers: {
-        "Content-Type": "image/png",
-        "Cache-Control": "no-cache"
+        "Content-Type":"image/png",
+        "Cache-Control":"no-cache"
       }
+
     });
 
   }
+
 }
