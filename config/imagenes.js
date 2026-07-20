23664d413d96dcbd7969b8cc1255d743a706795c
rrolcito <rrolcito@gmail.com>
2026-07-20 20:13:04 +0200

    Create imagenes.js

---
+++ config/imagenes.js
@@ -0,0 +1,59 @@
+export const imagenes = {
+  Maddie: {
+    avatar: [],
+    firma: [
+      "1.png",
+      "2.png"
+    ]
+  },
+
+  Roxanne: {
+    avatar: [],
+    firma: []
+  },
+
+  Kaori: {
+    avatar: [],
+    firma: []
+  },
+
+  Sophie: {
+    avatar: [],
+    firma: []
+  },
+
+  "Déjà": {
+    avatar: [],
+    firma: []
+  },
+
+  Lily: {
+    avatar: [],
+    firma: []
+  },
+
+  Noor: {
+    avatar: [],
+    firma: []
+  },
+
+  Eilin: {
+    avatar: [],
+    firma: []
+  },
+
+  Narcissa: {
+    avatar: [],
+    firma: []
+  },
+
+  Persephone: {
+    avatar: [],
+    firma: []
+  },
+
+  Jackie: {
+    avatar: [],
+    firma: []
+  }
+};
